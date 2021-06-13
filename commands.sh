41122205899910c1a1da8cabd218241117d9825a
mortal94-king <rh5650920@gmail.com>
2021-06-13 16:53:32 +0600

    Termux all commands

---
+++ commands.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+termux-setup-storage
+apt update
+apt upgrade
+pkg update
+pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pip install lolcat
+pip2 install requerment
+pkg install nano
+
+
